45abd58a972e2544b1bef4af72828139e59efb88
Abhishek Kumar <kumarabhishek7481087466@gmail.com>
2024-06-20 12:55:46 +0530

    first

--- variable1.py
+++ variable1.py
@@ -2,10 +2,12 @@ name="abhishek";
 age=20;
 age2=age;
 roll_no=34567890.234;
+char ='a';
 
 print("my name is :",name);
 print("my age :",age2);
 print("my roll_no is a ",roll_no);
+print(type(char))
 
 
 
